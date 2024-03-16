64c44f9eb8e389fa011745cadb948ace25c481bf
tiwariayush23 <125736456+tiwariayush23@users.noreply.github.com>
2024-03-16 17:01:42 +0000

    learn how to insert variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 144535
+let accountEmail = "tayush377@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+
+accountEmail = "hch@gmail.com"
+accountPassword = "212121"
+accountCity = "Bhopal"
+
+// accountID = 2 // not allowed
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
