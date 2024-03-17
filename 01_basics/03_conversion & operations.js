20cb585f3c2f2f06341444bd690a7024a6f73c56
tiwariayush23 <125736456+tiwariayush23@users.noreply.github.com>
2024-03-17 16:28:28 +0000

    operations used in js

--- 01_basics/03_conversion & operations.js
+++ 01_basics/03_conversion & operations.js
@@ -3,7 +3,7 @@ let score = true
 // console.log(typeof score)
 // console.log(typeof (score))
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 // console.log(typeof valueInNumber)
 // console.log(valueInNumber)
 
@@ -21,7 +21,39 @@ let valueInNumber = Number(score)
 // "" => false
 //  "ayush" = true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ******************* Operations ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+ 
+// let str1 = "hello"
+// let str2 = " ayush"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 +2)
+// console.log(1+ 2 +"2")
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
