8b101c0d5ae8e19c776f1243648745b760f3e5e3
tiwariayush23 <125736456+tiwariayush23@users.noreply.github.com>
2024-03-19 17:15:42 +0000

    Stack & Heap in js

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -3,19 +3,19 @@
 // 7 types : String, Number, Boolean, null, undefined
 //  Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
 
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
 
 
@@ -25,20 +25,38 @@ console.log(id === anotherId)
 
 // Array, Objects, Functions
 
-const heros = ["Shaktiman", "Naagraj", "Doga"] // Array
-console.log(heros)
-let myObj = { //objects
-    name: "Ayush",
-    age: 22
+// const heros = ["Shaktiman", "Naagraj", "Doga"] // Array
+// console.log(heros)
+// let myObj = { //objects
+//     name: "Ayush",
+//     age: 22
+// }
+// console.log(myObj)
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+
+// console.log(typeof heros)
+// console.log(typeof myObj)
+// console.log(typeof myFunction)
+// console.log(typeof anotherId)
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//  Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "Ayushtiwaridotcom"
+let anotherName = "Chai aur Code"
+console.log(myYoutubeName)
+console.log(anotherName)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
-console.log(myObj)
-const myFunction = function(){
-    console.log("Hello World");
-}
 
-console.log(typeof heros)
-console.log(typeof myObj)
-console.log(typeof myFunction)
-console.log(typeof anotherId)
-
-
+let userTwo = userOne
+userTwo.email = "ayush@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
