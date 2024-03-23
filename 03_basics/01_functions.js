c75dd54a563e321d657130c321f0466a30ad2ecd
Ayush Tiwari <125736456+tiwariayush23@users.noreply.github.com>
2024-03-23 17:24:23 +0000

    funtions of js in detail

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,35 @@ function loginUserMessage(username = "Sam"){
 }
 
 // console.log(loginUserMessage("Ayush"));
-console.log(loginUserMessage("Ayush"));
+// console.log(loginUserMessage("Ayush"));
+
+function calculateCartPrice (val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+// console.log(calculateCartPrice(200, 400, 500, 2500));
+
+const user = {
+    username: "Ayush",
+    prices: 199
+}
+
+function handleObject (anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price : 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,800]));
