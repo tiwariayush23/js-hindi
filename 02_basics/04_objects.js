7c812f391945b822d326a546dfee00b43b06ebfb
Ayush Tiwari <125736456+tiwariayush23@users.noreply.github.com>
2024-03-23 15:46:42 +0000

    Jason api intro in js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -48,10 +48,41 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+//  Destructing Objects
+const course = {
+    coursename :"JS in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+// JASON API FORMAT
+
+// {
+//     "name": "ayush",
+//     "coursename": "JS in hindi",
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+
+
