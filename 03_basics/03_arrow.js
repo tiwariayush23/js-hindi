066047a944ebdead0c0cb5baabdf25a68a7ca3ea
Ayush Tiwari <125736456+tiwariayush23@users.noreply.github.com>
2024-03-24 17:02:16 +0000

    arrow intro in js

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,24 @@
+const user = {
+    username: "ayush",
+    price:999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai () {
+//     let username = "ayush"
+//     console.log(this.name);
+// }
+
+// chai()
+
