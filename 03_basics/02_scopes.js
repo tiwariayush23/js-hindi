f6ce3955d55257a60727513256b3d91344f1452a
Ayush Tiwari <125736456+tiwariayush23@users.noreply.github.com>
2024-03-24 16:41:26 +0000

    more about scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,17 +1,57 @@
 // var c = 300
 let a = 300 // globalscope
 
-if (true) {
-    let a = 10 //block  scope
-    const b = 20
-    console.log("INNER: ",a);
-}
+// if (true) {
+//     let a = 10 //block  scope
+//     const b = 20
+//     console.log("INNER: ",a);
+// }
 
 // for (let i = 0; i < array.length; i++) {
 //     const element = array[i];
     
 // }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+    const username = "ayush"
+
+    function two () {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "ayush"
+    if(username === "ayush") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// *************************** Interesting *******************
+
+console.log(addone(5));
+function addone(num) {
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
