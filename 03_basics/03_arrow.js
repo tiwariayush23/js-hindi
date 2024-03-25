2006aed101be59b0fd7a2196caf8243808a07eef
Ayush Tiwari <125736456+tiwariayush23@users.noreply.github.com>
2024-03-25 07:26:23 +0000

    arrow function  and this keyword in js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -17,8 +17,34 @@ const user = {
 
 // function chai () {
 //     let username = "ayush"
-//     console.log(this.name);
+//     console.log(this.username);
 // }
 
 // chai()
 
+// const chai = function () {
+//     let username = "ayush"
+//     console.log(this.username);
+// }
+
+const chai =  () => {     // => arrow function declaration
+    let username = "ayush"
+    // console.log(this.username);
+    console.log(this);
+}
+
+
+// chai()
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2 // Implicit return
+// const addTwo = (num1, num2) =>  ( num1 + num2 )
+// const addTwo = (num1, num2) =>  {username: "Ayush"}
+const addTwo = (num1, num2) =>  ({username: "Ayush"})
+
+
+console.log(addTwo(5,6));
