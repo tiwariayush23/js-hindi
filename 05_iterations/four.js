e0cb5e1c4b7aedab8cac47508e4252608badb9cd
Ayush Tiwari <125736456+tiwariayush23@users.noreply.github.com>
2024-03-28 16:31:51 +0000

    for in loops & for of loops in js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -4,7 +4,23 @@ const myObject = {
     rb: "ruby",
     swift: "swift by apple"
 }
+// for in loops for object
 
-for (const key in myObject) {
+// for (const key in myObject) {
+//     console.log(`${key}, shortcut is for ${myObject[key]}`);
+// }
+
+// const programming = ["js", "rb", "py", "java", "c++"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+
+for (const key in map) {
     console.log(key);
 }
