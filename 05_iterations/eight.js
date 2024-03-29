69e28361011c7ccc755263b885f6f15f52ae1bfa
Ayush Tiwari <125736456+tiwariayush23@users.noreply.github.com>
2024-03-29 16:35:24 +0000

    reduce methods in js

---
+++ 05_iterations/eight.js
@@ -0,0 +1,36 @@
+//  Reduce 
+
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce(  (acc, curr) => acc + curr, 0   )
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "JS course",
+        price: 2999
+    },
+    {
+        itemName: "JAVA course",
+        price: 1999
+    },
+    {
+        itemName: "PY course",
+        price: 999
+    },
+    {
+        itemName: "Data Science course",
+        price: 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce(  (acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
